a6355f670ab8bd39e12e1156f2c05a97f2077724
BryantCR <bryancvargas20@gmail.com>
2021-11-15 10:17:03 -0600

    All ok

--- MaxMinAvg.js
+++ MaxMinAvg.js
@@ -1,32 +1,48 @@
 
-function basicOPerations(arr){
 
+arr = [1,-2,9,4]
 
+function Max(arr){
     var max = arr[0];
-    var sum = 0;
-    var min = arr[0];
-
     for(var i = 0; i<arr.length; i++){
         if(arr[i] > max){
             max = arr[i];
         }
-        return max;
+        
     }
+    return max;
+}
 
+function Min(arr){
+    var min = arr[0];
     for(var i = 0; i<arr.length; i++){
         if(arr[i] < min){
-            max = arr[i];
+            min = arr[i];
         }
-        return min;
+        
     }
-
-    for(var i = 0; i<arr.length; i++){
-        sum = sum + arr[i]
-    }
-    return sum / arr.length;
-
-    console.log();
-
+    return min;
 }
-arr = [1,-2,9,4]
-basicOPerations(arr);
+    
+
+function Average(arr){
+    var sum = 0;
+    for(var i = 0; i<arr.length; i++){
+        sum = sum + arr[i];
+    }
+    avg = sum / arr.length;
+    return avg;
+}
+
+function maxMinAverage(arr) {
+
+    var result = Max(arr);
+    console.log("Max: "+ result);
+    var result2 = Min(arr);
+    console.log("Min: "+ result2);
+    var result3 = Average(arr);
+    console.log("Average: "+ result3);
+    return 'The minimum is ' + result2 + ', the maximum is ' + result + ', and the average is ' + result3;
+}
+
+maxMinAverage(arr)
